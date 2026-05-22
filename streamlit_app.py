3c26a315abeb67dfd579287545e90b29e8496850
davinafaizaa <davinafaizaa@gmail.com>
2026-05-22 14:23:36 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 st.title("🎈 Davina's new app")
 st.write(
@@ -6,3 +7,9 @@ st.write(
 )
 color = st.color_picker("Pick A Color", "#000000")
 st.write("The current color is", color)
+
+from numpy.random import default_rng as rng
+
+df = pd.DataFrame(rng(0).standard_normal((20, 3)), columns=["a", "b", "c"])
+
+st.bar_chart(df)
