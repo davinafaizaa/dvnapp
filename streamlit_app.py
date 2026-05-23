7148b87cac3f8d88c378408c92ea473422ede24b
davinafaizaa <davinafaizaa@gmail.com>
2026-05-23 11:38:05 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,16 +13,18 @@ html, body, [class*="css"] {
 .custom-text {
     font-size: 30px;
     color: #232E75;
-    font-family: 'Poppins', sans-serif;
+    font-family: 'Poppins'
     font-weight: 600;
 }
 </style>
 """, unsafe_allow_html=True)
 
-
 st.title("🎈 Davina's new app")
-st.write('<p style="font-size:30px; color:#232E75; font-family:Courier New;">Color Picker</p>',
-         unsafe_allow_html=True)
+st.markdown(
+    '<p class="custom-text">Color Picker</p>',
+    unsafe_allow_html=True
+)
+
 color = st.color_picker("Pick A Color", "#F5DFEE")
 st.write("The current color is", color)
 
