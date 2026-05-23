22771080c09eac838bbaddf21c2832a88f2099eb
davinafaizaa <davinafaizaa@gmail.com>
2026-05-23 10:09:42 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,6 @@ st.title("🎈 Davina's new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
-font = "monospace"
+font = "Serif"
 color = st.color_picker("Pick A Color", "#F5DFEE")
 st.write("The current color is", color)
