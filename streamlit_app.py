244c7fc8ef9f8cb3072eab0aabd89f78ee328abf
davinafaizaa <davinafaizaa@gmail.com>
2026-05-23 10:43:59 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,9 @@
 import streamlit as st
 import pandas as pd
 
-@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap'); 
-html, body, [class*="css"] {
-    font-family: 'Roboto', sans-serif; 
-    font-size: 18px;
-    font-weight: 500;
-    color: #091747;
-
 st.title("🎈 Davina's new app")
-st.write('<p style="font-size:30px; color:#232E75; font:Serif">Color Picker</p>',
-unsafe_allow_html=True)
+st.write('<p style="font-size:30px; color:#232E75; style="font:Serif">Color Picker</p>',
+'unsafe_allow_html=True)
 color = st.color_picker("Pick A Color", "#F5DFEE")
 st.write("The current color is", color)
 
