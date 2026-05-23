0a22955f94cf85603651e5a09efb22952f7852d7
davinafaizaa <davinafaizaa@gmail.com>
2026-05-23 11:06:13 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,23 +1,8 @@
 import streamlit as st
 import pandas as pd
 
-def set_custom_font(font_url, font_name):
-    custom_css = f"""
-    <style>
-    @font-face {{
-        font-family: '{font_name}';
-        src: url('{font_url}');
-    }}
-    html, body, [class*="st-"] {{
-        font-family: '{font_name}', sans-serif;
-    }}
-    </style>
-    """
-    st.markdown(custom_css, unsafe_allow_html=True)
-
-# Contoh memuat font dari URL eksternal
-set_custom_font("https://fonts.google.com/", "Playwrite México Guides")
-
+with open( "app\style.css" ) as css:
+    st.markdown( f'<style>{css.read()}</style>' , unsafe_allow_html= True)
 
 st.title("🎈 Davina's new app")
 st.write('<p style="font-size:30px; color:#232E75;">Color Picker</p>',
