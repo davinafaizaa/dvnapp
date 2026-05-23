f1f7fb7de11b3d99ba02b7051cf2428d37043966
davinafaizaa <davinafaizaa@gmail.com>
2026-05-23 11:38:43 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,30 +1,9 @@
 import streamlit as st
 import pandas as pd
 
-# Import Google Font
-st.markdown("""
-<style>
-@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap');
-
-html, body, [class*="css"] {
-    font-family: 'Poppins', sans-serif;
-}
-
-.custom-text {
-    font-size: 30px;
-    color: #232E75;
-    font-family: 'Poppins'
-    font-weight: 600;
-}
-</style>
-""", unsafe_allow_html=True)
-
 st.title("🎈 Davina's new app")
-st.markdown(
-    '<p class="custom-text">Color Picker</p>',
-    unsafe_allow_html=True
-)
-
+st.write('<p style="font-size:30px; color:#232E75; font-family:Serif;">Color Picker</p>',
+         unsafe_allow_html=True)
 color = st.color_picker("Pick A Color", "#F5DFEE")
 st.write("The current color is", color)
 
