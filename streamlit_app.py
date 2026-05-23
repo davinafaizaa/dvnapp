cd9913ddd6eadbcf86f25cd462444f6052436d77
davinafaizaa <davinafaizaa@gmail.com>
2026-05-23 10:06:54 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,11 +8,5 @@ st.write(
 color = st.color_picker("Pick A Color", "#F5DFEE")
 st.write("The current color is", color)
 
-st.markdown(
-    f"""
-    <div style="font-family: 'Courier New', Courier, monospace; background-color: #f0f2f6; padding: 15px; border-radius: 5px;">
-        <pre>{kode}</pre>
-    </div>
-    """,
-    unsafe_allow_html=True
-)
+[theme]
+font = "monospace"
