7617fee5843023860d3ce46adcae9e0459d72e15
davinafaizaa <davinafaizaa@gmail.com>
2026-05-23 10:36:41 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 st.title("🎈 Davina's new app")
-st.write (#'Markdown')
+st.write ('##Markdown')
 st.write('<p style="font-size:30px; color:#232E75;">**Color Picker**</p>',
 unsafe_allow_html=True)
 color = st.color_picker("Pick A Color", "#F5DFEE")
