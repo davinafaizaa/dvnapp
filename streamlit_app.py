6c3fdadefbf6fea1a2e456c6d2cf19bd29335695
davinafaizaa <davinafaizaa@gmail.com>
2026-05-23 10:26:37 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,7 @@ st.title("🎈 Davina's new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
-color = st.color_picker.size50("Pick A Color", "#F5DFEE")
+color = st.color_picker("Pick A Color", "#F5DFEE")
 st.write("The current color is", color)
+st.background_color "F5DFEE"
 
