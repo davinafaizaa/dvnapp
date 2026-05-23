06311b66015687c18113f0983683e6cd266f66f0
davinafaizaa <davinafaizaa@gmail.com>
2026-05-23 10:24:09 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import pandas as pd
 
-theme.font "Serif"
+[theme]
+font="Serif"
 st.title("🎈 Davina's new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
