8137c5310093e6f9fd8d1bf95d60eee5141b74dd
davinafaizaa <davinafaizaa@gmail.com>
2026-05-23 10:56:40 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,4 +8,21 @@ color = st.color_picker("Pick A Color", "#F5DFEE")
 st.write("The current color is", color)
 
 
+import streamlit as st
 
+def set_custom_font(font_url, font_name):
+    custom_css = f"""
+    <style>
+    @font-face {{
+        font-family: '{font_name}';
+        src: url('{font_url}');
+    }}
+    html, body, [class*="st-"] {{
+        font-family: '{font_name}', sans-serif;
+    }}
+    </style>
+    """
+    st.markdown(custom_css, unsafe_allow_html=True)
+
+# Contoh memuat font dari URL eksternal
+set_custom_font("https://fonts.google.com/", "Playwrite México Guides")
